d1dee163314252e05fa79450d035747007b12fd9
BEYZA <byznr.okyar@gmail.com>
2023-07-03 18:56:26 +0300

    obje takibi dosyları eklendi

---
+++ obje_takibi.py
@@ -0,0 +1,45 @@
+import cv2
+import time
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+mpHand = mp.solutions.hands
+hands = mpHand.Hands() #ellerin tespiti ve konumlandaralmasa için 
+mpdraw = mp.solutions.drawing_utils #çizim
+
+pTime=0 #bunlar fps değerleri için
+cTime=0
+
+while True:
+    control, capture = cap. read ()
+    results = hands.process (capture)
+    
+    print(results.multi_hand_landmarks) #el tespiti kordinatlara
+
+    
+    if results.multi_hand_landmarks: #her dir landmarks içerisinde gezilir.
+     for handlms in results.multi_hand_landmarks: # handlms ile x,y ve z kordinatlara alinar.
+         mpdraw.draw_landmarks(capture, handlms, mpHand.HAND_CONNECTIONS)
+     
+         for number, lm in enumerate(handlms.landmark):
+             print(number, lm) #number eklem numarasa, lm=> x,y,2 kordinatlara
+             h,w,c = capture. shape
+             cx, cy = int (lm.x*w),int(lm.y*h)
+             
+             #isaret parmaginan tespiti
+             if ((number==4)|(number==8)|(number==12)| (number==16) | (number==20)):
+                cv2.circle(capture,(cx, cy),20,(255,0,0),cv2.FILLED)
+    #fps 
+    cTime=time.time()
+    fps = 1/(cTime-pTime)
+    pTime=cTime
+
+
+    cv2.putText(capture,"FPS: "+str(int(fps)),(10, 75), cv2.FONT_HERSHEY_PLAIN,3,(255, 0, 0),5) 
+    cv2.imshow("capture", capture)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+       break
+        
+cv2.destroyAllWindows
---
+++ video_okuma_ve_kamera.py
@@ -0,0 +1,19 @@
+import cv2 
+import time
+import mediapipe as mp
+
+
+def video_capture(bfr):
+    capture=cv2.VideoCapture(bfr)
+
+    while True: # her frame akması için için döngü oluşturuyoruz . frame de video içindeki resimler gibi düşün sahne yni
+        kontrol,frame=capture,read()
+        cv2.imshow("frame",frame)
+        if cv2.waitKey(1) & 0xFF==ord("q"):  # klavyemizde q ya basında video kaydı sonlanacak
+            break
+    cv2.destroyAllWindows()
+
+if __name__=='__main__':
+
+    bfr=0 # pc kameraası için sıfqqır
+    video_capture(bfr)
